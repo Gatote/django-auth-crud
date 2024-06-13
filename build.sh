3a9ef448e256ef02979d914b57906dfdb98ba294
Gatote <gatico6470@gmail.com>
2024-06-12 20:28:51 -0600

    a

--- build.sh
+++ build.sh
@@ -9,6 +9,3 @@ python manage.py collectstatic --no-input
 
 # Aplicar migraciones de la base de datos
 python manage.py migrate
-
-# Ejecutar Gunicorn
-gunicorn djangocrud.wsgi --bind 0.0.0.0:$PORT --workers 4 --log-level=info
