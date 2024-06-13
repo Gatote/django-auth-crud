9f70b6ee63177c479ea65107a9676c6bba2d7c98
Gatote <gatico6470@gmail.com>
2024-06-12 20:12:51 -0600

    subida final proyecto

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
 set -o errexit
 
-# poetry install
+# Instalación de dependencias
 pip install -r requirements.txt
 
+# Recolectar archivos estáticos
 python manage.py collectstatic --no-input
+
+# Aplicar migraciones de la base de datos
 python manage.py migrate
+
+# Ejecutar Gunicorn
+gunicorn djangocrud.wsgi --bind 0.0.0.0:$PORT --workers 4 --log-level=info
